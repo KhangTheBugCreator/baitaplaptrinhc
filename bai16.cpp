a3cb15ff248a1d69534d2b9c24958f5aff333455
Nguyễn Minh Khang <110124083@st.tvu.edu.vn>
2024-09-25 19:42:57 +0700

    Add files via upload

---
+++ bai16.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+int main(){
+	char kitu;
+	scanf("%c",&kitu);
+	if ((kitu >='A') && (kitu <= 'Z'))
+		printf("YES");
+	else 
+		printf("NO");
+	
+	printf("\n%d",kitu);
+	
+	return 0;
+}
